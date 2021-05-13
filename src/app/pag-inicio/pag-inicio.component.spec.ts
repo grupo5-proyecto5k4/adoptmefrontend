fc52143623b6006bd73ace64f9543ed9c08145af
julieta <julietascrepnik@gmail.com>
2021-05-13 17:12:21 -0300

    cambios de inicio

--- src/app/pag-inicio/pag-inicio.component.spec.ts
+++ src/app/pag-inicio/pag-inicio.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {MatButtonModule} from '@angular/material/button';
 import { PagInicioComponent } from './pag-inicio.component';
 
 describe('PagInicioComponent', () => {
