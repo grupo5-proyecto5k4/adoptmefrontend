7923d1fae65d07f07e05dc299192b0efb318ad3e
tamara.doffo <tamara.doffo@ross.com.ar>
2021-05-31 20:25:10 -0300

    test12

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/adoptmefrontend'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
@@ -28,5 +28,5 @@ const forceSSL = function() {
   // For all GET requests, send back index.html
   // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + 'index.html'));
+    res.sendFile(path.join(__dirname + '/dist/adoptmefrontend/index.html'));
   });
