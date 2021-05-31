6a6d03b70eb11c67c6ee6c97c2ab2547036cb7c6
tamara.doffo <tamara.doffo@ross.com.ar>
2021-05-31 20:14:38 -0300

    test11

--- server.js
+++ server.js
@@ -28,5 +28,5 @@ const forceSSL = function() {
   // For all GET requests, send back index.html
   // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + 'index.html'));
   });
