a186b4b2d03e5aaa5997b9f62bb4319b71f564c8
tamara.doffo <tamara.doffo@ross.com.ar>
2021-05-31 20:06:54 -0300

    test10

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
@@ -24,4 +25,8 @@ const forceSSL = function() {
   // to use the forceSSL
   // middleware
   app.use(forceSSL());
-  
+  // For all GET requests, send back index.html
+  // so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+  });
