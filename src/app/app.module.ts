34a4fe32d0a38102ad01b99ff07502664947122e
milagrosmore <milimore16@gmail.com>
2021-06-03 12:25:03 -0300

    adding under-construction page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,12 +15,14 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { ComponentsModule } from 'src/app/components/components.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { UnderConstructionComponent } from './under-construction/under-construction.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LandingPageComponent,
+    UnderConstructionComponent,
   ],
   imports: [
     BrowserModule,
