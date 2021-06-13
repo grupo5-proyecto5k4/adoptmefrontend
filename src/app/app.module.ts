21ffa718bd53be84359c1e18d6255f16870dd5ea
julieta <julietascrepnik@gmail.com>
2021-06-13 10:58:05 -0300

    se agrega contacto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { UnderConstructionComponent } from './under-construction/under-construct
 import { FaqsComponent } from './faqs/faqs.component';
 import { TermsAndConditionsComponent } from './terms-and-conditions/terms-and-conditions.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 
 
@@ -35,6 +36,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
     FaqsComponent,
     TermsAndConditionsComponent,
     NotFoundComponent,
+    ContactoComponent,
 
   ],
   imports: [
