b4ed9c286393a99aa4f804e3b09c34298b13ede8
tamara.doffo <tamara.doffo@ross.com.ar>
2021-06-21 23:53:40 -0300

    testing integration7

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   //base_url: '/api',
-  base_url: '',
+  base_url: 'https://adoptmebackend.herokuapp.com',
   //user: {base_url: '/user'}, 
-  user: {base_url: '/registro'},
-  host: 'https://adoptmebackend.herokuapp.com'
+  user: {base_url: '/registro'}
+ 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,9 @@ export const environment = {
   //production: false
   production: true,
   //base_url: '/api',
-  base_url: '',
+  base_url: 'https://adoptmebackend.herokuapp.com',
   //user: {base_url: '/user'}, 
-  user: {base_url: '/registro'},
-  host: 'https://adoptmebackend.herokuapp.com'
+  user: {base_url: '/registro'}
 };
 
 /*
