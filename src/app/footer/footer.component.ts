cf9a5620dfecddc2bd4b131adf1d0d5e81c378d3
julieta <julietascrepnik@gmail.com>
2021-07-11 16:22:05 -0300

    cambios en footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -23,8 +23,9 @@ export class FooterComponent {
   
 
   goToHome(){
-    this.dialog.open(LandingPageComponent).updatePosition({ bottom:'627px'})    
+    //this.dialog.open(LandingPageComponent).updatePosition({ bottom:'627px'})    
      //document.getElementById("quienesomos").scrollTo(0, 0);
+     document.getElementById("quienesomos").scrollIntoView();
   }
 
   openTermsAndConditions(){
