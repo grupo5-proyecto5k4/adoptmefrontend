09966f1775b9cd313b08187b05c88451f1e4b1aa
julieta <julietascrepnik@gmail.com>
2021-07-13 14:14:09 -0300

    se agrega boton cancelar a inicio

--- src/app/inicio-sesion/inicio-sesion.component.ts
+++ src/app/inicio-sesion/inicio-sesion.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-inicio-sesion',
-  templateUrl: './inicio-sesion.component.html',
+  templateUrl:'./inicio-sesion.component.html',
   styleUrls: ['./inicio-sesion.component.scss']
 })
 export class InicioSesionComponent {
