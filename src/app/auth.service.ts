0153e5935394abfdc8ed46d5d9c9c46534277890
julieta <julietascrepnik@gmail.com>
2021-07-14 13:59:31 -0300

    se arregla mensajes de error en login

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { Data, Router } from '@angular/router';
 import {AlertsService} from '../utils/alerts.service';
-import { AnyCnameRecord } from 'dns';
+
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +16,7 @@ export class AuthService {
   login(email: string, password: string) {
     debugger;
     this.http.post(this.api + '/login', {correoElectronico: email,contrasenia: password})
-    .subscribe((resp:any) => {
+    .subscribe((resp:Data) => {
       this.router.navigate(['landing']);
       localStorage.setItem('auth_token', resp.token);
       this.alertsService.confirmMessage("Inicio de sesión exitoso");
