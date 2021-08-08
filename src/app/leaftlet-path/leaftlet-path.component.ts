136305658a8324c006a51c44dfcb0d2fbfeb3d47
milagrosmore <milimore16@gmail.com>
2021-08-08 00:24:50 -0300

    markers ejemplos

--- src/app/leaftlet-path/leaftlet-path.component.ts
+++ src/app/leaftlet-path/leaftlet-path.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import * as Leaflet from 'leaflet';
 
-var greenIcon = Leaflet.icon({
-  iconUrl: 'assets/images/leaflet/marker-icon.png',
+var redIcon = Leaflet.icon({
+  iconUrl: 'assets/images/leaflet/red-icon.png',
   iconSize:     [25, 41], // size of the icon
-  shadowSize:   [50, 64], // size of the shadow
-  iconAnchor:   [7.5, 30], // point of the icon which will correspond to marker's location
-  shadowAnchor: [12.5, 41],  // the same for the shadow
+  iconAnchor:   [12.5, 41], // point of the icon which will correspond to marker's locatio
+  popupAnchor:  [0, -35] // point from which the popup should open relative to the iconAnchor
+});
+
+var blueIcon = Leaflet.icon({
+  iconUrl: 'assets/images/leaflet/blue-icon.png',
+  iconSize:     [25, 41], // size of the icon
+  iconAnchor:   [12.5, 41], // point of the icon which will correspond to marker's locatio
   popupAnchor:  [0, -35] // point from which the popup should open relative to the iconAnchor
 });
 
@@ -14,10 +19,13 @@ var locations = [
   ["LOCATION_1", -31.41666919552361, -64.1879917385005],
 ]
 
-var planes = [
-  ["Veterinaria Martinez",-40.99497,174.50808],
-  ["Veterinaria Martinez<br><a href='www.google.com'>Google</a>",-41.30269,173.63696],
-  ["7C6CA1",-41.49413,173.5421]
+var centrosCastracion = [
+  ["<strong>Centro de Castración Municipal</strong><br>Dr. Francisco Muñiz 60<br>",-31.407387, -64.209818]
+]
+
+var veterinaria = [
+  ["<strong>Veterinaria Alem</strong><br>Blvd. San Juan 125<br><a href='https://www.veterinariaalem.com/'>Sitio Web</a>",-31.424622, -64.182666],
+  ["<strong>Medicina Felina</strong><br>Lopez de Vega 408",-31.382872, -64.179952]
   ];
 @Component({
   selector: 'app-leaftlet-path',
@@ -29,7 +37,7 @@ export class LeaftletPathComponent implements OnInit, OnDestroy {
   map: Leaflet.Map;
 
   ngOnInit(): void {
-    this.map = Leaflet.map('map').setView([-41.3058, 174.82082], 8);
+    this.map = Leaflet.map('map').setView([-31.411156, -64.191211], 13);
 
 
     Leaflet.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -41,8 +49,12 @@ export class LeaftletPathComponent implements OnInit, OnDestroy {
     accessToken: 'pk.eyJ1IjoibWlsaW1vcmUxNjE2IiwiYSI6ImNrcmlpdTRsNDB2aXozMW80MTQwa3YxemwifQ.bzPgST4tdgMA5loxAX-eew'
 }).addTo(this.map);
 
-  for (var i = 0; i < planes.length; i++) {
-    var marker = new Leaflet.marker([planes[i][1], planes[i][2]]).bindPopup(planes[i][0])
+  for (var i = 0; i < veterinaria.length; i++) {
+    var marker = new Leaflet.marker([veterinaria[i][1], veterinaria[i][2]], {icon: blueIcon}).bindPopup(veterinaria[i][0])
+      .addTo(this.map);
+  }
+  for (var i = 0; i < centrosCastracion.length; i++) {
+    var marker = new Leaflet.marker([centrosCastracion[i][1], centrosCastracion[i][2]], {icon: redIcon}).bindPopup(centrosCastracion[i][0])
       .addTo(this.map);
   }
     //var marker = Leaflet.marker([-31.41666919552361, -64.1879917385005], {icon: greenIcon}).addTo(this.map).bindPopup('Veterinaria Martinez<br>').openPopup();
