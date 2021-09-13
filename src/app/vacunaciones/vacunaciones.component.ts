e0eb63b7cb563364f66a66df5a9d2e1e408f7b52
julieta <julietascrepnik@gmail.com>
2021-09-13 18:46:33 -0300

    se agrega vacunacion

--- src/app/vacunaciones/vacunaciones.component.ts
+++ src/app/vacunaciones/vacunaciones.component.ts
@@ -9,13 +9,6 @@ import {Router, ActivatedRoute, Data} from '@angular/router';
 import {MatTableDataSource} from '@angular/material/table';
 
 
-export interface Element {
-  name: string;
-  fecha: string;
-  dosis: string;
-  ProximaDosis: string;
-}
-
 @Component({
   selector: 'app-vacunaciones',
   templateUrl: './vacunaciones.component.html',
@@ -24,26 +17,42 @@ export interface Element {
 export class VacunacionesComponent implements OnInit {
   Titulo="Registro de Vacunaciones";
   SignupForm: FormGroup;
-  displayedColumns = ['name', 'fecha', 'dosis', 'ProximaDosis'];
-  datasource;
+  
 
   constructor(private dialog:MatDialog) { }
 
-  ngOnInit(): void {
+  columnas: string[] = ['nombre', 'fecha', 'dosis','proxDosis'];
 
-    const ELEMENT_DATA: Element[] = [
-      {name: 'Hydrogen', fecha:'djnw', dosis: 'H',ProximaDosis:'dss'},
-      
-      
-    ];
+  
+  dataSource = null;
 
-    this.datasource = new MatTableDataSource(ELEMENT_DATA);
+  ngOnInit() {
 
+    this.SignupForm= new FormGroup({
+      nombreVac:new FormControl('',Validators.required),
+      fecha: new FormControl('',Validators.required),
+      dosis: new FormControl('',Validators.required),
+      proxDosis: new FormControl('',Validators.required),
+    });
+    
+    
   }
 
-  
+  registrarVacuna(){
+    //vacunas:Vacuna[] = [new Vacuna('papas',11,'sadfa',55),];
 
+    this.dataSource = new MatTableDataSource();
+  }
   
-  
-
+  filtrar(event: Event) {
+    const filtro = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filtro.trim().toLowerCase();
+  }  
 }
+
+export class Vacuna {
+  constructor(public nombre: string, public fecha: Date, public dosis: string, public proxDosis: Date) {
+  }
+}
+
+
