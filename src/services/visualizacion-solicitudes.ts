7b9b82be438d111493b7f99d14aa99e70bee212b
grupo5-proyecto5k4 <81835477+grupo5-proyecto5k4@users.noreply.github.com>
2021-10-05 22:50:00 -0300

    se sube abrir solicitud desde la notificacion

--- src/services/visualizacion-solicitudes.ts
+++ src/services/visualizacion-solicitudes.ts
@@ -10,6 +10,7 @@ export class VisualizacionSolicitudesService {
 
     endpointSolicitudesAdoptar='https://adoptmebackend.herokuapp.com/formulario/buscar/solicitudadopcion/adopcion';
     endpointSolicitudesProvisorio='https://adoptmebackend.herokuapp.com/formulario/buscar/solicitudadopcion/provisorio';
+    endpointGetSolicitudes='https://adoptmebackend.herokuapp.com/formulario/adopcion';
 
     constructor(private http: HttpClient) {}
 
@@ -19,5 +20,9 @@ export class VisualizacionSolicitudesService {
 
     getSolicitudesProvisorio(token:string): Observable<any> {
         return this.http.get((this.endpointSolicitudesProvisorio), { headers: new HttpHeaders().set('auth-token', `${token}`)});
+    }
+
+    async getSolicitud(idSolictud: string): Promise<any> {
+        return this.http.get((this.endpointGetSolicitudes+'/'+idSolictud)).toPromise();
     }
 }
