4edc45ad88a400986b10a16923bf08af0f60b98f
milagrosmore <milimore16@gmail.com>
2021-10-11 15:42:12 -0300

    adding listado solicitud provi

---
+++ src/app/visualizacion-solicitud-provi/visualizacion-solicitud-provi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visualizacion-solicitud-provi',
+  templateUrl: './visualizacion-solicitud-provi.component.html',
+  styleUrls: ['./visualizacion-solicitud-provi.component.scss']
+})
+export class VisualizacionSolicitudProviComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
