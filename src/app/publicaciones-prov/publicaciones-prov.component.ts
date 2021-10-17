a4f8163e6442a619be7544d167c38d1f387db2e5
grupo5-proyecto5k4 <81835477+grupo5-proyecto5k4@users.noreply.github.com>
2021-10-16 23:27:05 -0300

    se suben filtros

--- src/app/publicaciones-prov/publicaciones-prov.component.ts
+++ src/app/publicaciones-prov/publicaciones-prov.component.ts
@@ -1,12 +1,12 @@
-
-import { Component, OnInit, OnDestroy, ViewChild, ChangeDetectorRef} from '@angular/core';
-import { MatPaginator} from '@angular/material/paginator';
+import { Component, OnInit, OnDestroy, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Observable } from 'rxjs';
 import { Mascota } from 'src/models/IMascota';
 import { MatDialog } from '@angular/material/dialog';
-import { RegistroMascotasService} from 'src/services/registro-mascotas.service';
+import { RegistroMascotasService } from 'src/services/registro-mascotas.service';
 import { VerMascotaComponent } from '../components/ver-mascota/ver-mascota.component';
+import { FormControl, FormGroup } from '@angular/forms';
 
 export interface Pet {
   name: string;
@@ -17,17 +17,17 @@ export interface Pet {
 
 
 const DATA: Pet[] = [
-  {name: "Pepe", age: 3, esCachorro: "Adulto", sexo: "Hembra"},
-  {name: "Limon", age: 2,  esCachorro: "Cachorro", sexo: "Macho"},
-  {name: "Teodoro", age: 1,  esCachorro: "Adulto", sexo: "Hembra"},
-  {name: "Zeus", age: 2,  esCachorro: "Cachorro", sexo: "Macho"},
-  {name: "Jamón", age: 2,  esCachorro: "Cachorro", sexo: "Macho"},
-  {name: "Messi", age: 1,  esCachorro: "Adulto", sexo: "Hembra"},
-  {name: "Totin", age: 2,  esCachorro: "Cachorro", sexo: "Macho"},
-  {name: "Jose", age: 2,  esCachorro: "Cachorro", sexo: "Macho"},
-  {name: "Ragnar", age: 1,  esCachorro: "Adulto", sexo: "Hembra"},
-  {name: "Osiris", age: 2,  esCachorro: "Cachorro", sexo: "Macho"},
-  {name: "Jack", age: 5,  esCachorro: "Cachorro", sexo: "Macho"}
+  { name: "Pepe", age: 3, esCachorro: "Adulto", sexo: "Hembra" },
+  { name: "Limon", age: 2, esCachorro: "Cachorro", sexo: "Macho" },
+  { name: "Teodoro", age: 1, esCachorro: "Adulto", sexo: "Hembra" },
+  { name: "Zeus", age: 2, esCachorro: "Cachorro", sexo: "Macho" },
+  { name: "Jamón", age: 2, esCachorro: "Cachorro", sexo: "Macho" },
+  { name: "Messi", age: 1, esCachorro: "Adulto", sexo: "Hembra" },
+  { name: "Totin", age: 2, esCachorro: "Cachorro", sexo: "Macho" },
+  { name: "Jose", age: 2, esCachorro: "Cachorro", sexo: "Macho" },
+  { name: "Ragnar", age: 1, esCachorro: "Adulto", sexo: "Hembra" },
+  { name: "Osiris", age: 2, esCachorro: "Cachorro", sexo: "Macho" },
+  { name: "Jack", age: 5, esCachorro: "Cachorro", sexo: "Macho" }
 ]
 
 @Component({
@@ -38,60 +38,141 @@ const DATA: Pet[] = [
 
 
 export class PublicacionesProvComponent implements OnInit {
-  mascotasPubProvisorio: any;
-  mascotasPub:any;
+  mascotasPubProvisorio = [];
+  FilterForm: FormGroup;
+  tamanos = ['Pequeño','Mediano','Grande'];
+  gatoSeleccionado = false;
+  filtroAplicado = false;
 
   /*
   @ViewChild(MatPaginator) paginator: MatPaginator;
   obs: Observable<any>;
   dataSource: MatTableDataSource<Pet> = new MatTableDataSource<Pet>(DATA);
   */
-  constructor(public registroMascotasService:RegistroMascotasService, private dialog: MatDialog, private changeDetectorRef: ChangeDetectorRef) {
+  constructor(public registroMascotasService: RegistroMascotasService, private dialog: MatDialog, private changeDetectorRef: ChangeDetectorRef) {
   }
 
   ngOnInit() {
-    /*
-    this.changeDetectorRef.detectChanges();
-    this.dataSource.paginator = this.paginator;
-    this.obs = this.dataSource.connect();
-    this.paginator._intl.itemsPerPageLabel = "Animales por página";
-    */
+
+    this.iniciarForm();
+
     // En provisorio
     this.registroMascotasService.getMascotas(2).subscribe(dataOne => {
-      this.mascotasPub = dataOne;
 
       // "En adopcion y en provisorio"
       this.registroMascotasService.getMascotas(0).subscribe(dataBoth => {
-        
-        // Junto los de "En provisorio" con los de "En adopción y en provisorio"
-            var data = [].concat(dataBoth, dataOne);
-            this.mascotasPubProvisorio = data;
-            //Recorro mascotas
-            for (let x = 0; x < (data.length); x++){
-              // Edad 
-              this.mascotasPubProvisorio[x].edad = this.calculateAge(data[x].fechaNacimiento);
-              if (data[x].Foto.length != 0){
-                //Recorro imágenes
-                for (let i = 0; i < data[x].Foto.length; i++){
-                  // Foto Principal
-                  if (data[x].Foto[i].esPrincipal){
-                    this.mascotasPubProvisorio[x].imagenCard = data[x].Foto[i].foto;
-                  }
-                }
-              }
+        this.unirMascotas(dataBoth, dataOne)
+      }
+      )
+
+    },
+      err => {
+
+      }
+    )
+  }
+
+
+
+  changeTipoAnimal(){
+    if (this.FilterForm.controls.tipoMascota.value == 1){
+      this.gatoSeleccionado = true;
+    }
+    else{
+      this.gatoSeleccionado = false;
+    }
+  }
+
+  iniciarForm(){
+    this.FilterForm = new FormGroup({
+      nombre: new FormControl(''),
+      tipoMascota: new FormControl(''),
+      tamanoFinal: new FormControl(''),
+      sexo: new FormControl(''),
+      barrio: new FormControl(''),
+    });
+  }
+
+  unirMascotas(dataBoth: any, dataOne: any) {
+    this.mascotasPubProvisorio = [];
+    // Junto los de "En adopción" con los de "En adopción y en provisorio"
+    let data = undefined;
+    if (dataBoth && dataOne) {
+      data = [].concat(dataBoth, dataOne);
+    }
+    else if(dataBoth[0]._id){
+      data = dataBoth;
+    }
+    else{
+      data = dataOne;
+    }
+    this.mascotasPubProvisorio = data;
+    //Recorro mascotas
+    console.log("cantidad de mascotas a mostrar:")
+    console.log(this.mascotasPubProvisorio)
+    if (this.mascotasPubProvisorio.length > 0) {
+      for (let x = 0; x < (this.mascotasPubProvisorio.length); x++) {
+        // Edad 
+        this.mascotasPubProvisorio[x].edad = this.calculateAge(data[x].fechaNacimiento);
+        if (data[x].Foto.length > 0) {
+          //Recorro imágenes
+          for (let i = 0; i < data[x].Foto.length; i++) {
+            // Foto Principal
+            if (data[x].Foto[i].esPrincipal) {
+              this.mascotasPubProvisorio[x].imagenCard = data[x].Foto[i].foto;
             }
           }
-          )
-          
-    },
-    err => {
-      console.log('ERROR...')
+        }
+      }
     }
+  }
+
+
+  async buscar() {
+    this.filtroAplicado = true;
+    let filters: any = {};
+    if (this.FilterForm.controls.nombre.value !== '') {
+      filters.nombres = this.FilterForm.controls.nombre.value;
+    }
+    if (this.FilterForm.controls.barrio.value !== '') {
+      filters.barrio = this.FilterForm.controls.barrio.value;
+    }
+    if (this.FilterForm.controls.tamanoFinal.value !== '') {
+      filters.tamañoFinal = this.FilterForm.controls.tamanoFinal.value;
+    }
+    if (this.FilterForm.controls.sexo.value !== '') {
+      filters.sexo = this.FilterForm.controls.sexo.value;
+    }
+    if (this.FilterForm.controls.tipoMascota.value !== '') {
+      filters.tipoMascota = this.FilterForm.controls.tipoMascota.value;
+    }
+
+    filters.estado = "Disponible Provisorio";
+    this.registroMascotasService.getMascotasFiltradas(filters).subscribe(dataOne => {
+      console.log(dataOne);
+
+      // "En adopcion y en provisorio"
+      filters.estado = "Disponible Adopción y Provisorio";
+      this.registroMascotasService.getMascotasFiltradas(filters).subscribe(dataBoth => {
+        console.log(dataBoth);
+        this.unirMascotas(dataBoth, dataOne);
+      })
+    },
+      err => {
+
+      }
     )
+    window.scrollTo(0, 0);
+  }
+
+
+
+  clean() {
+    this.iniciarForm();
   }
   /*
   ngOnDestroy() {
-    if (this.dataSource) { 
+        if (this.dataSource) { 
       this.dataSource.disconnect(); 
     }
   }
@@ -101,20 +182,20 @@ export class PublicacionesProvComponent implements OnInit {
     var fechaNacimientoFormato = new Date(fechaNacimiento);
     var difference = (today.getTime() - fechaNacimientoFormato.getTime()) / (1000 * 60 * 60 * 24);
     var sms: String;
-    if (difference < 365){
+    if (difference < 365) {
       sms = "Cachorro"
     } else {
       sms = "Adulto"
     }
     return sms
-}
+  }
 
-openMascota(mascota: Mascota){
-  this.dialog.open(VerMascotaComponent, {
-    data: {
+  openMascota(mascota: Mascota) {
+    this.dialog.open(VerMascotaComponent, {
+      data: {
         mascota: mascota,
         accion: 0
-    }
-})
-}
+      }
+    })
+  }
 }
