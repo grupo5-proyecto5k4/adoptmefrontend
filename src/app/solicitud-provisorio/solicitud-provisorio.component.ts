23f4b30731a3811347f01658a396725a4fa1e0d5
grupo5-proyecto5k4 <81835477+grupo5-proyecto5k4@users.noreply.github.com>
2021-10-18 13:03:28 -0300

    se soluciona notificacion provi y adopcion (primera que se envia)

--- src/app/solicitud-provisorio/solicitud-provisorio.component.ts
+++ src/app/solicitud-provisorio/solicitud-provisorio.component.ts
@@ -265,7 +265,8 @@ export class SolicitudProvisorioComponent implements OnInit {
 
       
       this.userService.registrarFormularioProvisorio(formulario, this.authService.getToken()).subscribe((resp:Data) => {
-        this.notificacionService.notificarSolicitudProvisorio(this.data.mascota.nombreMascota,this.data.mascota.responsableId, resp._id, this.authService.getToken())
+        console.log(this.data.mascota.nombreMascota+" "+resp._id+" "+this.data.mascota.responsableId);
+        this.notificacionService.notificarSolicitudProvisorio(this.data.mascota.nombreMascota,resp._id,this.data.mascota.responsableId, this.authService.getToken())
         this.alertsService.confirmMessage("Su solicitud de provisorio ha sido registrada").then((result) => window.location.href = '/');        
       },
         error => {
