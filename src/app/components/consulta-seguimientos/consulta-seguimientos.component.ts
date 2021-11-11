5e953d12e3523d8b217847be36537a81e6b37d79
grupo5-proyecto5k4 <81835477+grupo5-proyecto5k4@users.noreply.github.com>
2021-11-11 11:43:17 -0300

    se sube avances visualizacion visita

--- src/app/components/consulta-seguimientos/consulta-seguimientos.component.ts
+++ src/app/components/consulta-seguimientos/consulta-seguimientos.component.ts
@@ -12,11 +12,15 @@ import { VisualizacionSolicitudesService } from 'src/services/visualizacion-soli
   styleUrls: ['./consulta-seguimientos.component.scss']
 })
 export class ConsultaSeguimientosComponent implements OnInit {
-  seguimientos = [];
+  seguimientos: any;
   accion: any;
   mascota: any;
   proceso = "";
   solicitud: any;
+  fotos: any = [];
+  fotoVisualizar: any = [];
+  slideIndex = 0;
+
   constructor(public dialog: MatDialog, @Inject(MAT_DIALOG_DATA) public data: any, private visualizarService: VisualizacionSolicitudesService, private mascotaService: MascotaService, private notificacionService: NotificacionService, private authService: AuthService) { }
 
   ngOnInit(): void {
@@ -33,6 +37,38 @@ export class ConsultaSeguimientosComponent implements OnInit {
     else {
       this.proceso = "Cancelar provisorio"
     }
+
+
+    if (this.seguimientos.length > 0) {
+      if (this.seguimientos.Visita.length > 0){
+      for (let x = 0; x < (this.seguimientos.Visita.length); x++) {
+        // Edad 
+        if (this.seguimientos.Visita[x].visitaFotos.length > 0) {
+          //Recorro imágenes
+          for (let i = 0; i < this.seguimientos.Visita[x].visitaFotos.length; i++) {
+            // Foto Principal
+         //   if (this.seguimientos.Visita[x].visitaFotos[i].esPrincipal) {
+              this.seguimientos.Visita[x].imagenCard = this.seguimientos.Visita[x].Foto[i].foto;
+
+              const object1 = {
+                path: this.seguimientos.Visita[x].imagenCard
+              };
+              if (this.seguimientos.Visita[x].imagenCard.esPrincipal) {
+                this.seguimientos.Visita[x].fotos.unshift(object1);
+                this.fotoVisualizar = [];
+                this.fotoVisualizar.push(object1);
+              }
+              else {
+                this.seguimientos.Visita[x].push(object1);
+              }
+
+         //   }
+          }
+        }
+      }
+    }
+    }
+
 
   }
 
@@ -55,6 +91,35 @@ export class ConsultaSeguimientosComponent implements OnInit {
     });
 
   }
+
+  // Next/previous controls
+  plusSlides(action: number) {
+
+    this.slideIndex + action
+    let object1 = {};
+    if ((this.slideIndex + action) >= 0 && (this.slideIndex + action) < this.fotos.length) {
+      object1 = {
+        path: this.fotos[this.slideIndex + action].path,
+      };
+      this.slideIndex += action;
+    }
+    else if (action == 1) {
+      object1 = {
+        path: this.fotos[0].path,
+      };
+      this.slideIndex = 0;
+    }
+    else{
+      object1 = {
+        path: this.fotos[this.fotos.length - 1].path,
+      };
+      this.slideIndex = this.fotos.length - 1;
+    }
+
+    this.fotoVisualizar = [];
+    this.fotoVisualizar.push(object1);
+  }
+
 
 
   async enviarNotificacionDeBaja() {
