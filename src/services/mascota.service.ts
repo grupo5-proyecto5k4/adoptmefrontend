6ae46c33a8aef35598e8a1d2f33f066d0ff93534
milagrosmore <milimore16@gmail.com>
2021-11-13 21:05:28 -0300

    adding seguimiento

--- src/services/mascota.service.ts
+++ src/services/mascota.service.ts
@@ -42,4 +42,8 @@ export class MascotaService {
   finalizarProvisorio(idAnimal: string, token: string, motivo: any): Observable <any>{
     return this.httpClient.put('/seguimiento/modificarSeguimiento/'+idAnimal, motivo, { headers: new HttpHeaders().set('auth-token', `${token}`) });
   }
+
+  finalizarSeguimiento(idSolicitud: string, token: string): Observable <any>{
+    return this.httpClient.put(this.api + '/seguimiento/finalizar/seguimiento/'+ idSolicitud, "some-string", { headers: new HttpHeaders().set('auth-token', `${token}`) });
+  }
 }
